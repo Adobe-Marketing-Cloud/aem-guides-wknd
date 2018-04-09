b7000e9668b8d0aa424b8e38d354990f44982ba8
Daniel Gordon <dgordon@adobe.com>
2018-04-09 12:19:20 -0400

    mobile navigation implementation

--- ui.apps/src/main/content/jcr_root/apps/wknd/clientlibs/clientlib-site/components/header/header.js
+++ ui.apps/src/main/content/jcr_root/apps/wknd/clientlibs/clientlib-site/components/header/header.js
@@ -1,16 +1,17 @@
 (function (site, $) {
     'use strict';
     var wkndheader = $(site + " .wknd-header"),
-        scroll;
-    
-    if($(window).scrollTop() > 30) {
+        scroll,
+        mobileBreakpoint = 992;
+     
+    if($(window).scrollTop() > 0 && $(window).width() > mobileBreakpoint) {
         wkndheader.addClass("navbar-sticky");
     }
-    
+     
     $(window).scroll(function(){
-         
+          
          scroll = $(window).scrollTop();
-    if(scroll > 30) {
+    if(scroll > 0 && $(window).width() > mobileBreakpoint) {
         wkndheader.addClass("navbar-sticky");
     } else {
         wkndheader.removeClass("navbar-sticky");
